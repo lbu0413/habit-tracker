d30b75f255c2409b948db779cfc5054bf5e9b692
LBU <lbu@LBUui-MacBook-Pro.local>
2021-04-05 22:41:01 -0400

    navbar and habitForm created

--- src/app.jsx
+++ src/app.jsx
@@ -13,11 +13,45 @@ class App extends Component {
     ],
 };
 
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => {
+        return item.id !== habit.id
+    })
+    this.setState({ habits })
+  };
+
+  handleAdd = name => {
+    const habits = [...this.state.habits, {id: Date.now(), name: name, count: 0}];
+    this.setState({ habits });
+  }
+
   render() {
     return(
       <>
-        <Navbar habits={this.state.habits} />
-        <Habits habits={this.state.habits} />
+        <Navbar totalCount={this.state.habits.filter((item) => item.count > 0).length}/>
+        <Habits 
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd} 
+        />
       </>
     )
   }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 
 class Navbar extends Component {
     render() {
-        const { habits } = this.props;
-
         return(
-            <nav>
-                
+            <nav className="navbar">
+                <i className="navbar-logo fas fa-leaf"></i>
+                <span>Habit Tracker</span>
+                <span className="navbar-count">{this.props.totalCount}</span>
             </nav>
         )
     }
